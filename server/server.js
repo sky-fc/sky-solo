3648ec5a9df612c39e06f30546ae34b9cd0d7e9c
Sky Fuzell-Casey <skyfc.work@gmail.com>
2023-12-11 12:38:16 -0800

    Initial commit

---
+++ server/server.js
@@ -0,0 +1,88 @@
+// server/server.js
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+const app = express();
+const port = 5000;
+
+app.use(cors());
+app.use(express.json());
+
+// Connect to MongoDB
+mongoose.connect('mongodb://localhost:27017/crud_app', { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', () => {
+    console.log('Connected to MongoDB');
+});
+
+// Define the item schema
+const itemSchema = new mongoose.Schema({
+    name: String,
+});
+
+const Item = mongoose.model('Item', itemSchema);
+
+// Get all items
+app.get('/api/items', async (req, res) => {
+    try {
+        const items = await Item.find();
+        res.json(items);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
+
+// Get a single item by ID
+app.get('/api/items/:id', async (req, res) => {
+    try {
+        const item = await Item.findById(req.params.id);
+        if (item) {
+            res.json(item);
+        } else {
+            res.status(404).json({ message: 'Item not found' });
+        }
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
+
+// Add a new item
+app.post('/api/items', async (req, res) => {
+    const newItem = new Item({
+        name: req.body.name,
+    });
+
+    try {
+        const savedItem = await newItem.save();
+        res.status(201).json(savedItem);
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+});
+
+// Update an item
+app.put('/api/items/:id', async (req, res) => {
+    try {
+        const updatedItem = await Item.findByIdAndUpdate(req.params.id, req.body, { new: true });
+        res.json(updatedItem);
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+});
+
+// Delete an item
+app.delete('/api/items/:id', async (req, res) => {
+    try {
+        await Item.findByIdAndRemove(req.params.id);
+        res.json({ message: 'Item deleted successfully' });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
